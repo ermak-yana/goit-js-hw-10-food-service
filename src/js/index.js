47012db5c6984326f106c6d9947c46624ffc4024
ermak-yana <lidl.peperts.stor@gmail.com>
2021-08-14 22:17:59 +0300

    менюшка

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
-import './css/styles.css';
+import data from './menu.json';
+import '../css/styles.css';
+
+import theme from './switch-theme.js';
+import menuItem from '../templates/menu-items.hbs';
+
+
+const menuList = document.querySelector('.js-menu');
+const switchToggle = document.querySelector('.theme-switch__toggle');
+const body = document.querySelector('body');
+
+
+const result = data.map(item => menuItem(item)).join('');
+menuList.insertAdjacentHTML('beforeend', result);
